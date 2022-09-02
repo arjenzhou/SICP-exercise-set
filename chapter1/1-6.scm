914b07d8d0bb4e514ccabcb4657a6e254df998bb
Zhou Yang <zhouyang.zy@outlook.com>
2022-09-02 10:11:18 +0800

    chore: delete redundant function

--- chapter1/1-6.scm
+++ chapter1/1-6.scm
@@ -1,15 +1,5 @@
 (load "Square-Roots-by-Newton_s-Method.scm")
 
-(define (good-enough? guess x)
-	(< (abs (- (square guess) x)) 0.001))
-
-(define (improve guess x)
-	(average guess (/ x guess)))
-
-(define (average a b)
-	(/ (+ a b) 2))
-;;;
-
 (define (new-if predicate then-clause else-clause)
 	(cond (predicate then-clause)
 		(else else-clause)))
